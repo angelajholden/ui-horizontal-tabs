9f4e83e107dc94c54cc1de034d13d846fecb3da5
Angela J. Holden <angelajholden@gmail.com>
2020-12-03 19:42:52 -0600

    adding a README

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function horizontalTabs() {
 
     //listen for the click on the button container
     buttons.addEventListener("click", (e) => {
-        //if the target has a .button class, then add the .active class
+        //if the target has a .button class, then add .active class
         if (e.target.classList.contains("button") === true) {
             e.target.classList.add("active");
         }
@@ -27,18 +27,18 @@ function horizontalTabs() {
                 let contentId = content_item[x].dataset.id;
                 let buttonId = button_item[i].dataset.id;
 
-                //if the button is clicked AND the IDs match, add active class
+                //if the button is clicked AND the IDs match, add .active class
                 if (button === e.target && contentId === buttonId) {
                     //log the matching IDs
                     // console.log(contentId + buttonId);
                     content.classList.add("active");
                 }
 
-                //if the button was not clicked, remove active class
+                //if the button was not clicked, remove .active class
                 if (button !== e.target) {
                     button.classList.remove("active");
 
-                    //and if the IDs match, remove active class
+                    //and if the IDs match, remove .active class
                     if (contentId === buttonId) {
                         content.classList.remove("active");
                     }
